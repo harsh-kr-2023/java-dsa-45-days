e73114ced7c13229d939b5a61febcd2cf21c5e45
Harsh Kumar <hk0008390@gmail.com>
2025-11-20 21:27:06 +0530

    Day 6 code

---
+++ 20-11-25/MergeSort.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class MergeSort {
+    public static void mergeSort(int[] arr, int n) {
+        if (n < 2) {
+            return;
+        }
+        int low = 0;
+        int high = n - 1;
+        int mid = (low + high) / 2;
+        merge(arr, low, mid, high);
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[low + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = low;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < n1) {
+            arr[k++] = left[i++];
+        }
+        while (j < n2) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        mergeSort(arr, n);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
